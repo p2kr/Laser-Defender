712ac15d55da0b08ac0de1113dfaed23a0ae47b8
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-11 13:48:40 +0530

    Spawn Multiple Enemies

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    // config params
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] float moveSpeed = 2f;
+
+    // 
+    int waypointIndex = 0;
+    List<Transform> waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition,
+                movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
