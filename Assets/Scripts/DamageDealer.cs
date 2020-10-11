356f96b114753fe5ef9a6fcb55847420789944df
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-10-11 14:50:11 +0530

    Destroy on zero health

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int damage = 100;
 
-    // Update is called once per frame
-    void Update()
+    public int GetDamage() { return damage; }
+
+    public void Hit()
     {
-        
+        Destroy(gameObject);
     }
 }
